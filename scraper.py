7ae3bda693d58525a77a3ddf9940510b88663982
aorosoeon <arsen.chuz@gmail.com>
2024-11-11 00:15:22 -0500

    implemented pagination, now it opens every product link on every page

--- scraper.py
+++ scraper.py
@@ -11,13 +11,37 @@ browser = webdriver.Chrome(service=service) #creating a new instance of chromedr
 
 def get_initial_window():
     browser.get(url)
-    sleep(1)
-    #browser.add_cookie({"name": "ssUserId", "value": ss_user_id})
+    sleep(2)
+    browser.add_cookie({"name": "ssUserId", "value": ss_user_id})
     browser.add_cookie({"name": "OptanonConsent", "value": optanon_consent})
     browser.add_cookie({"name": "OptanonAlertBoxClosed", "value": optanon_alert_box_closed})
-    sleep(1)
+    sleep(2)
     browser.get(url)
     sleep(5)
     WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.TAG_NAME, "body"))) #"wait until" to ensure it's loaded
 
+def js_click(browser, button, seconds): #there are different ways of "clicking" something with selenium. when you click something with click() (your see your cursor moves), linkedin can intercept this click and the program will crash. it's linkedin's way of preventing bots. that's why we need to click with javascript - it's a more reliable way of clicking
+    WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.TAG_NAME, "body"))) #making sure that the page is loaded. if it's not and you will try to click, the program will crash
+    browser.execute_script("arguments[0].click();", button) #clicking the element with javascript
+    sleep(seconds) #wait to ensure that there is a pause between click and next action
+
+def next_page():
+    next_page = browser.find_element(By.XPATH, "//a[@class='action  next' and contains(@ng-href, 'https://www.sail.ca/en/footwear/men?page=')]")
+    js_click(browser, next_page, 2) #click send_wo_note button
+    sleep(4)
+    WebDriverWait(browser, 5).until(EC.presence_of_element_located((By.TAG_NAME, "body")))
+    browser.back()
+    sleep(4)
+
 get_initial_window()
+
+page_num = 1
+
+while True:
+    browser.get(f"{url}?page={page_num}")
+    sleep(5)
+    product_links = [link.get_attribute("href") for link in browser.find_elements(By.TAG_NAME, "a") if link.get_attribute("class") == "product-item-link ng-binding"]
+    for product_link in product_links:
+        browser.get(product_link)
+        sleep(5)
+    page_num = page_num + 1
